58dab2a48554fb3137fddd2c60031b65a7e335d5
Afontivero <44619954+Afontivero@users.noreply.github.com>
2018-11-28 18:54:30 +0000

    Add files via upload

---
+++ GetEmailAddresses.ps1
@@ -0,0 +1,5 @@
+﻿###Replace searchbase with distinguished name e.g. ou=users,dn=contoso,dn=com
+###Replace FilePath with Location of file including the file name you want created and extension
+###Must import-module activedirectory and have admin rights for this to run
+
+Get-ADUser -SearchBase "OU distinguished name here" -Filter{Enabled -eq $True} -Properties mail | out-file -FilePath "C:\temp\FileName.csv"
